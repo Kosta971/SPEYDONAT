410d803217d08e80b8e519d174625b299c727dd4
Kosta971 <rgsjcsn@gmail.com>
2025-05-27 23:35:55 +0300

    Update backend/main.py

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,10 @@
+from fastapi import FastAPI
+from .routes import users, donations
+from .database import Base, engine
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+app.include_router(users.router)
+app.include_router(donations.router)
 
